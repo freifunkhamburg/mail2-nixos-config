8856b812bcb539bef9101c4dea2d7df4bdc57c4b
Daniel Frank <git@danielfrank.net>
2019-06-10 14:45:56 +0200

    minor cleanup

--- sshusers.nix
+++ sshusers.nix
@@ -13,7 +13,7 @@
 let
   sshkeys = pkgs.fetchFromGitHub {
     owner = "freifunkhamburg";
-    "repo" = "ssh-keys";
+    repo = "ssh-keys";
     rev = "70a8f1a4b8ddf921579986fb08b45050abeef2bc";
     sha512 = "05p3ypg5imjxiswsspiix1l783w11ddby78bwjv0dnppbz8i4ddiy8fz70vcz4q2fbb94kwnk5zm7mz53h24z3j97xq9d485nmxinpq";
   };
