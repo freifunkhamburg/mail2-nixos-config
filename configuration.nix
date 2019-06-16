a18802f9dbc409068ac1cd4414e720507c4b9820
Daniel Frank <git@danielfrank.net>
2019-06-16 20:48:13 +0200

    enable screen and set a decent screenrc

--- configuration.nix
+++ configuration.nix
@@ -67,13 +67,18 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    git htop lsof nano sqlite tcpdump traceroute vim wget
+    git htop lsof nano screen sqlite tcpdump traceroute vim wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
   # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  programs.screen.screenrc = ''
+    hardstatus alwayslastline
+    hardstatus string '%{= kG}[ %{G}%H %{g}][%= %{= kw}%?%-Lw%?%{r}(%{W}%n*%f%t%?(%u)%?%{r})%{w}%?%+Lw%?%?%= %{g}][%{B} %m-%d %{W}%c:%s %{g}]'
+    defscrollback 1000
+  '';
 
   # List services that you want to enable:
 
