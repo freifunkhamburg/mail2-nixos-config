5fd35ba767f43d05c9077c6a083aa9d722c876cd
Daniel Frank <git@danielfrank.net>
2022-01-02 19:51:59 +0100

    Use niv to include other git repos

--- mailserver.nix
+++ mailserver.nix
@@ -1,12 +1,5 @@
 { config, pkgs, ... }:
 
-let mymailserver = (import <nixpkgs> {}).pkgs.fetchgit {
-    url = "https://codeberg.org/tokudan/nixos-mailserver.git";
-    rev = "4ace785a05f233392b6db6c82dcdd25599cd88dc";
-    sha256 = "1cwj2k2w7316jby7l7fjcclbwjfs7p7ymyslw1cfh7y8vf2a32ii";
-  };
-in
-
 {
   # Import some configuration as they are too long to be easily readable here
   imports = [ 
@@ -15,7 +8,7 @@ in
     #./postfixadmin.nix
     #./roundcube.nix
     #./rspamd.nix
-    "${mymailserver}/default.nix"
+    (import (import ./nix/sources.nix).nixos-mailserver)
   ];
   networking.domain = "hamburg.freifunk.net";
   services.mymailserver = {
